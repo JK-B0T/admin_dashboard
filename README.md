a119b12bfc291e2db846ce2d09c6c5104e2bd9bf
JK-B0T <103139241+JK-B0T@users.noreply.github.com>
2024-04-27 13:25:45 +0200

    Create initial files with boilerplate content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # admin_dashboard
+
+This project purpose is to test and demonstrate the skills I have gather up with the CSS grid display.
---
+++ script.js
@@ -0,0 +1,5 @@
+window.document.addEventListener("DOMContentLoaded", main, false);
+
+function main () {
+    console.log("Script loaded!");
+}
